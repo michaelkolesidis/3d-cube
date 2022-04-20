e8dcec6d65d3c8af8913fdd08490a1c2bbc24de7
Michael Kolesidis <michael.kolesidis@gmail.com>
2022-04-20 21:20:53 +0300

    cube

---
+++ 3d-cube.js
@@ -0,0 +1,82 @@
+// Initial angles of rotation
+let thetaX = 0;
+let thetaY = 0;
+let thetaZ = 0;
+
+// Rotation speed
+let rotationSpeed = 0.01;
+
+// Cube size
+let t = 100
+
+function setup() {
+	createCanvas(windowWidth - (0.4*windowWidth/100), windowHeight- (0.4*windowHeight/100), WEBGL);
+	smooth(8);
+}
+
+function draw() {
+    translate(-width/2,-height/2)
+	background(0);
+	rectMode(CENTER);
+	noStroke();
+	
+	// Save the transformation state
+	push();
+	translate(width / 2, height / 2, 0);
+	rotateX(thetaX);
+	rotateY(thetaY);
+	rotateZ(thetaZ);
+	
+	// Draw the sides of the cube and apply color
+	beginShape();
+	
+	// front side
+	fill(color(0xff, 0x14, 0xeb));
+	vertex(-t, -t, 0);
+	vertex(t, -t, 0);
+	vertex(t, t, 0);
+	vertex(-t, t, 0);
+	
+	// right side
+	fill(color(0x14, 0xf7, 0xff));
+	vertex(t, -t, 0);
+	vertex(t, -t, -2 * t);
+	vertex(t, t, -2 * t);
+	vertex(t, t, 0);
+	
+	// back side
+	fill(color(0xff, 0x14, 0xeb));
+	vertex(t, -t, -2 * t);
+	vertex(t, t, -2 * t);
+	vertex(-t, t, -2 * t);
+	vertex(-t, -t, -2 * t);
+	
+	// left side
+	fill(color(0x14, 0xf7, 0xff));
+	vertex(-t, -t, -2 * t);
+	vertex(-t, t, -2 * t);
+	vertex(-t, t, 0);
+	vertex(-t, -t, 0);
+	
+	// bottom side
+	fill(color(0x14, 0xff, 0x3b));
+	vertex(-t, t, 0);
+	vertex(t, t, 0);
+	vertex(t, t, -2 * t);
+	vertex(-t, t, -2 * t);
+	
+	// top side
+	fill(color(0xf3, 0xff, 0x14));
+	vertex(-t, -t, 0);
+	vertex(t, -t, 0);
+	vertex(t, -t, -2 * t);
+	vertex(-t, -t, -2 * t);
+	endShape();
+	
+	// Resore the transformation state
+	pop();
+	
+	thetaX = map(mouseX, 0, width, 0, TWO_PI);
+	thetaY = map(mouseY, 0, height, 0, TWO_PI);
+	thetaZ = map((mouseX + mouseY) / 2, 0, (width + height) / 2, 0, TWO_PI);
+}
